4784747edc37ee82c32c35db7a5804984cb78733
evuz <jesus_jgb_45@hotmail.com>
2018-12-07 17:20:44 +0100

    reset.css

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import * as serviceWorker from './serviceWorker';
 
 import { App } from './components/App';
 
+import './assets/reset.css';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
