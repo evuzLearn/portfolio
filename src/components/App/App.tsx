a98dce19360fa7b7a87b9223fe08e98806c2a20b
evuz <jesus_jgb_45@hotmail.com>
2018-12-08 10:27:43 +0100

    feat: include button in app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,18 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 
 import logo from '../../assets/logo.svg';
+import { Button } from '../Button';
 
 import './App.css';
 
 export function App() {
   const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>You've clicked {count} times on the button</p>
-        <button onClick={() => setCount(count + 1)}>Click me!</button>
+    <div className="app">
+      <header className="app--header">
+        <img src={logo} className="app--logo" alt="logo" />
+        <p className="app--text">You've clicked {count} times on the button</p>
+        <Button onClick={() => setCount(count + 1)}>Click me!</Button>
       </header>
     </div>
   );
